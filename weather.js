33a0b53cf9f8bd6124c30c4a6f99dd712d23f8b7
user <user>
2016-04-13 00:15:50 +0800

    - use XPath parse XML data. - add radio button to choose get data method.(xml/json) - use immediate function. - remove unnecessary css style.

--- weather.js
+++ weather.js
@@ -1,61 +1,105 @@
+(function(){
+
 var tmp={
+	defpage:"days",
+	type:"xml",
 	currentCity:"London",
 	currentDays:7,
 	month:["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 };
 
 $(document).ready(function(){
+	$("input:radio[name='typeSel'][value='"+tmp.type+"']").prop('checked',true);
 	$("#NextDays").addClass("tab_selected");
 	$("#changeDays").val(tmp.currentDays);
-	getNextDaysData(tmp.currentDays);
+	getNextDaysData(tmp.currentDays, tmp.type);
 	$("#content_next").show();
 	$("#CurrentCity").click(function(){
 		$("#NextDays").removeClass("tab_selected");
 		$("#CurrentCity").addClass("tab_selected");
-		getCurrentCityData(tmp.currentCity);
+		tmp.defpage="city";
+		getCurrentCityData(tmp.currentCity, tmp.type);
 		$("#content_next").hide();
 	});
 	$("#NextDays").click(function(){
 		$("#CurrentCity").removeClass("tab_selected");
 		$("#NextDays").addClass("tab_selected");
 		$("#content_city").hide();
-		getNextDaysData(tmp.currentDays);
+		tmp.defpage="days";
+		getNextDaysData(tmp.currentDays, tmp.type);
 	});
 	$("#changeCity").val(tmp.currentCity);
 	$("#changeDays").change(function(){
 		$(this).attr("disabled", true);
 		tmp.currentDays=$(this).val();
-		getNextDaysData($(this).val());
+		getNextDaysData($(this).val(), tmp.type);
 	});
 	$("#changeCity").change(function(){
 		$(this).attr("disabled", true);
 		tmp.currentCity=$(this).val();
-		getCurrentCityData($(this).val());
-	})
+		getCurrentCityData($(this).val(), tmp.type);
+	});
+	$("input:radio[name='typeSel']").change(function(){
+		if($(this).val() == "xml"){
+			tmp.type="xml";
+			if(tmp.defpage=="days"){
+				getNextDaysData(tmp.currentDays, "xml");
+			}else{
+				getCurrentCityData(tmp.currentCity, "xml");
+			}
+		}else{
+			tmp.type="json";
+			if(tmp.defpage=="days"){
+				getNextDaysData(tmp.currentDays, "json");
+			}else{
+				getCurrentCityData(tmp.currentCity, "json");
+			}
+		}
+	});
 });
 
-function getNextDaysData(days){
-	var apiURL = "http://api.openweathermap.org/data/2.5/forecast/daily?q="+tmp.currentCity+"&mode=xml&units=metric&cnt="+days+"&appid=5df396cf7b4d7b339bc250ebfd041f2e";
+function getNextDaysData(days, type){
+	var apiURL = "http://api.openweathermap.org/data/2.5/forecast/daily?q="+tmp.currentCity+"&mode="+type+"&units=metric&cnt="+days+"&appid=8b5e25806eda1463bbfb1ce83b50c4e9";
 	$.get(apiURL,function(data){
 		showNextDays(data, days);
 	});
 }
 
 function showNextDays(data, days){
+
+	if(tmp.type=="xml"){
+		var path = "/weatherdata/forecast";
+		var nodes = data.evaluate(path, data, null, XPathResult.ANY_TYPE, null);
+		var result = nodes.iterateNext();
+	}
+
 	var html="";
 	html += '<table class="table">';
 	html += '<tbody>';
 	for(var i=0;i<days;i++){
-		var getDay = $(data).find("time").eq(i).attr("day");
-		var mon = tmp.month[parseInt((getDay.split("-")[1]))-1];
-		var day = getDay.split("-")[2];
-		var icon = $(data).find("symbol").eq(i).attr("var");
-		var temperatureMax = Math.round($(data).find("temperature").eq(i).attr("max"));
-		var temperatureMin = Math.round($(data).find("temperature").eq(i).attr("min"));
-		var weatherDes = $(data).find("symbol").eq(i).attr("name");
-		var speed = $(data).find("windSpeed").eq(i).attr("mps");
-		var cloudsAll = $(data).find("clouds").eq(i).attr("all");
-		var pressure = $(data).find("pressure").eq(i).attr("value");
+		if(tmp.type=="xml"){
+			var getDay = result.childNodes[i].getAttributeNode("day").nodeValue;
+			var mon = tmp.month[parseInt((getDay.split("-")[1]))-1];
+			var day = getDay.split("-")[2];
+			var icon = result.childNodes[i].childNodes[0].getAttributeNode("var").nodeValue;
+			var temperatureMax = Math.round(result.childNodes[i].childNodes[4].getAttributeNode("max").nodeValue);
+			var temperatureMin = Math.round(result.childNodes[i].childNodes[4].getAttributeNode("min").nodeValue);
+			var weatherDes = result.childNodes[i].childNodes[0].getAttributeNode("name").nodeValue;
+			var speed = result.childNodes[i].childNodes[3].getAttributeNode("mps").nodeValue;
+			var cloudsAll = result.childNodes[i].childNodes[7].getAttributeNode("all").nodeValue;
+			var pressure = result.childNodes[i].childNodes[5].getAttributeNode("value").nodeValue;
+		}else{
+			var monTmp = new Date((data.list[i].dt)*1000).getMonth();
+			var mon = tmp.month[monTmp];
+			var day = new Date((data.list[i].dt)*1000).getDate();
+			var icon = data.list[i].weather[0].icon
+			var temperatureMax = Math.round(data.list[i].temp.max);
+			var temperatureMin = Math.round(data.list[i].temp.min);
+			var weatherDes = data.list[i].weather[0].description;
+			var speed = data.list[i].speed;
+			var cloudsAll = data.list[i].clouds;
+			var pressure = data.list[i].pressure;
+		}
 		html += '<tr>';
 		html += '<td>'+day+' '+mon+' <img src="images/'+icon+'.png"></td>';
 		html += '<td><span class="label label-warning">'+temperatureMax+'°C </span>&nbsp;';
@@ -69,49 +113,98 @@ function showNextDays(data, days){
 	$("#changeDays").attr("disabled", false);
 }
 
-function getCurrentCityData(city){
-	var apiURL = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&mode=xml&units=metric&appid=b1b15e88fa797225412429c1c50c122a";
+function getCurrentCityData(city, type){
+	var apiURL = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&mode="+type+"&units=metric&appid=8b5e25806eda1463bbfb1ce83b50c4e9";
 	$.get(apiURL,function(data){
 		showCurrentCity(data);
 	});
 }
 
 function showCurrentCity(data){
+	
 	var c = new Date();
-	var cityName = $(data).find("city").attr("name");
-	var country = $(data).find("country").text();
-	var temperature = Math.round($(data).find("temperature").attr("value"));
-	var icon = $(data).find("weather").attr("icon");
-	var getCityDes = $(data).find("weather").attr("number");
-	var cityDes;
-	switch(getCityDes.slice(0,1)){
-		case "2": cityDes="Thunderstorm"; break;
-		case "3": cityDes="Drizzle"; break;
-		case "5": cityDes="Rain"; break;
-		case "6": cityDes="Snow"; break;
-		case "8": 
-			getCityDes=="800" ? cityDes="Clear":cityDes="Clouds";
-			break;
-		default:
-			var getCityDesVal=$(data).find("weather").attr("value");
-			var strlen=getCityDesVal.length;
-			var firstChar=getCityDesVal.slice(0,1).toUpperCase();
-			cityDes=firstChar+getCityDesVal.slice(1,strlen);
-			break;
+
+	if(tmp.type=="xml"){
+		var path = "/current";
+		var citypath = "/current/city";
+		var windpath = "/current/wind";
+		var nodes = data.evaluate(path, data, null, XPathResult.ANY_TYPE, null);
+		var citynodes = data.evaluate(citypath, data, null, XPathResult.ANY_TYPE, null);
+		var windnodes = data.evaluate(windpath, data, null, XPathResult.ANY_TYPE, null);
+		var result = nodes.iterateNext();
+		var cityresult = citynodes.iterateNext();
+		var windresult = windnodes.iterateNext();
+
+		var cityName = cityresult.getAttributeNode("name").nodeValue;
+		var country = cityresult.childNodes[1].childNodes[0].nodeValue;
+		var temperature = Math.round(result.childNodes[1].getAttributeNode("value").nodeValue);
+		var icon = result.childNodes[8].getAttributeNode("icon").nodeValue;
+		var getCityDes = result.childNodes[8].getAttributeNode("number").nodeValue;
+		var cityDes;
+		switch(getCityDes.slice(0,1)){
+			case "2": cityDes="Thunderstorm"; break;
+			case "3": cityDes="Drizzle"; break;
+			case "5": cityDes="Rain"; break;
+			case "6": cityDes="Snow"; break;
+			case "8": 
+				getCityDes=="800" ? cityDes="Clear":cityDes="Clouds";
+				break;
+			default:
+				var getCityDesVal=result.childNodes[8].getAttributeNode("value").nodeValue;
+				var strlen=getCityDesVal.length;
+				var firstChar=getCityDesVal.slice(0,1).toUpperCase();
+				cityDes=firstChar+getCityDesVal.slice(1,strlen);
+				break;
+		}
+		var speedName = windresult.childNodes[0].getAttributeNode("name").nodeValue;
+		var speedVal = windresult.childNodes[0].getAttributeNode("value").nodeValue;
+		var directionName = windresult.childNodes[2].getAttributeNode("name").nodeValue;
+		var directionVal = windresult.childNodes[2].getAttributeNode("value").nodeValue;
+		var cloudsName = result.childNodes[5].getAttributeNode("name").nodeValue;
+		var pressureVal = result.childNodes[3].getAttributeNode("value").nodeValue;
+		var humidityVal = result.childNodes[2].getAttributeNode("value").nodeValue;
+		var getSunrise = new Date(cityresult.childNodes[2].getAttributeNode("rise").nodeValue);
+		var getSunset = new Date(cityresult.childNodes[2].getAttributeNode("set").nodeValue);
+		var sunrise = getSunrise.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: false});
+		var sunset = getSunset.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: false});
+		var coordlon = cityresult.childNodes[0].getAttributeNode("lon").nodeValue;
+		var coordlat = cityresult.childNodes[0].getAttributeNode("lat").nodeValue;
+	}else{
+		var cityName = data.name;
+		var country = data.sys.country;
+		var temperature = Math.round(data.main.temp);
+		var icon = data.weather[0].icon;
+		var getCityDes = data.weather[0].id;
+		var cityDes;
+		switch(getCityDes.toString().slice(0,1)){
+			case "2": cityDes="Thunderstorm"; break;
+			case "3": cityDes="Drizzle"; break;
+			case "5": cityDes="Rain"; break;
+			case "6": cityDes="Snow"; break;
+			case "8": 
+				getCityDes=="800" ? cityDes="Clear":cityDes="Clouds";
+				break;
+			default:
+				var getCityDesVal=data.weather[0].description;
+				var strlen=getCityDesVal.length;
+				var firstChar=getCityDesVal.slice(0,1).toUpperCase();
+				cityDes=firstChar+getCityDesVal.slice(1,strlen);
+				break;
+		}
+		var speedName = "---";
+		var speedVal = data.wind.speed;
+		var directionName = "---";
+		var directionVal = data.wind.deg;
+		var cloudsName = "---";
+		var pressureVal = data.main.pressure;
+		var humidityVal = data.main.humidity;
+		var getSunrise = new Date((data.sys.sunrise)*1000);
+		var getSunset = new Date((data.sys.sunset)*1000);
+		var sunrise = getSunrise.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: false});
+		var sunset = getSunset.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: false});
+		var coordlon = data.coord.lon;
+		var coordlat = data.coord.lat;
 	}
-	var speedName = $(data).find("speed").attr("name");
-	var speedVal = $(data).find("speed").attr("value");
-	var directionName =  $(data).find("direction").attr("name");
-	var directionVal =  $(data).find("direction").attr("value");
-	var cloudsName = $(data).find("clouds").attr("name");
-	var pressureVal = $(data).find("pressure").attr("value");
-	var humidityVal = $(data).find("humidity").attr("value");
-	var getSunrise = new Date($(data).find("sun").attr("rise"));
-	var getSunset = new Date($(data).find("sun").attr("set"));
-	var sunrise = getSunrise.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: false});
-	var sunset = getSunset.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit', hour12: false});
-	var coordlon = $(data).find("coord").attr("lon");
-	var coordlat = $(data).find("coord").attr("lat");
 
 	var html;
 	html =  '<h3>'+cityName+', '+country+'</h3>'+
@@ -163,3 +256,5 @@ function showCurrentCity(data){
 	$("#content_city").show();
 	$("#changeCity").attr("disabled", false);
 }
+
+}());
